8a2dc5bef2a3af995d0f8a6257de1bfd13e979ee
ShanmugamPalaniappan <87571486+ShanmugamPalaniappan@users.noreply.github.com>
2024-02-20 19:31:25 +0530

    Delete ecommerce/springboot/Repository directory

--- ecommerce/springboot/Repository/Photorepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.ecommerce.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.ecommerce.Model.Photo;
-
-public interface Photorepo extends JpaRepository<Photo, Integer>{
-
-}
